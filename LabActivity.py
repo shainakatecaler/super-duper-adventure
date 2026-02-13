aedd81c12f78bc8611f5c3422e9eb06f23796c01
shainakatecaler <cshainakate@gmail.com>
2026-02-13 08:38:53 +0800

    Create LabActivity.py

---
+++ LabActivity.py
@@ -0,0 +1,8 @@
+# declare variables and get user input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# print greeting message
+
+print("Hello! My name is ", name + "!")
+print("I am ", age, "years old.")
